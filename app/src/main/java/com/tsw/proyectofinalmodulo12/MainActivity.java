0daf6f07eccce66625ee418e327fe61873f6cd5e
Mauricio Alpizar C <ealpizar@cyberfuel.com>
2016-09-09 00:23:28 -0600

    Incorporacion al repositorio y creacion del Proyecto en GIT.

---
+++ app/src/main/java/com/tsw/proyectofinalmodulo12/MainActivity.java
@@ -0,0 +1,13 @@
+package com.tsw.proyectofinalmodulo12;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
